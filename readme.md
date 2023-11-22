e2150080d4d8113ae144d77f46c786b7bc8ad0da
amirDev00 <92927623+amirmahdimaleki@users.noreply.github.com>
2023-11-22 16:06:05 +0330

    last

---
+++ readme.md
@@ -0,0 +1,3 @@
+to get posts :
+http://localhost:5000/api/v1/posts
+hit the above route 🤷‍♂️
--- src/Config/config.ts
+++ src/Config/config.ts
@@ -7,37 +7,11 @@ const username = process.env.username
 const password = process.env.password
 
 
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = `mongodb+srv://${username}:${password}@cluster0.m621cln.mongodb.net/?retryWrites=true&w=majority`
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
-
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
-
 
 dotenv.config()
 
 // ^ mongo DB configuration
 const mongoURI = `mongodb+srv://amir123:amirr123@cluster0.m621cln.mongodb.net/?retryWrites=true&w=majority`
-
 const options = {
     autoIndex: false, 
     maxPoolSize: 10, 
